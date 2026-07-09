2e28aaae717b0184aabc079a6971cbab4819ea05
Ravi Pawar <pawarravi5674@gmail.com>
2026-07-09 14:36:39 +0530

    Add files via upload

---
+++ Range.py
@@ -0,0 +1,57 @@
+#wap to print all even number from 1-10
+# for num  in range(2,11,2):
+#     print(num)
+
+# #wap to print all odd numbers from 22-44
+# for num in range(23,45,2):
+#     print(num)
+
+#wap to print all even numbers from 100-50
+# for num in range(100,49,-2):
+#     print(num)
+
+
+#wap to print total count of element in given list
+# number=[10,20,30,40,50,60,70]
+# count=0
+# for num in number:
+#     count=count+1
+# print(count)
+
+#wap to print total addition of  all element in given list
+# number=[10,20,30,40,50,60,70]
+# count=0
+# for num in number:
+#     count=count+num
+# print(count)
+
+#wap to print total count of all even numbern from 1-100
+# count=0
+# for num in range(2,101,2):
+#     count=count+1
+# print(count)
+
+#wap to print total sum of all numbers from 1-100
+# total=0
+# for num in range(1,101,1):
+#   total=total+num
+# print(total)
+
+#wap to calculate total add of odd number from 1-100
+# count=0
+# for num in range(1,101,2):
+#     count=count+num
+# print(count)
+
+#wap to calculate total add of even number from 1-100
+# count=0
+# for num in range(2,101,2):
+#     count=count+num
+# print(count)
+
+#wap to get input from user and print the output using for loop
+# start=int(input('enter start value:'))
+# stop=int(input('enter stop value:'))
+# step=int(input("enter step value:"))
+# for num in range(start,stop+1,step):
+#     print(num)
