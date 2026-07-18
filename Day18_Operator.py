454d3697fa2adefdab0637a0cb8afff781ffd335
Ravi Pawar <pawarravi5674@gmail.com>
2026-07-18 19:54:57 +0530

    Add files via upload

---
+++ Day18_Operator.py
@@ -0,0 +1,108 @@
+#operator are a special symbol or character which is used to perform operation on data
+#----Arithmatic operator
+'''
++ , _ , * , / , // , % ,**
+'''
+
+# n1=15
+# n2=10
+# print(n1%n2)
+# print(n1*n2)
+
+
+#---comparision operator/relation operator
+'''
+>,<,<=,>=,==,!=
+'''
+
+# n1=15
+# n2=10
+# print(n1<n2)
+# print(n1!=n2)
+# print(n1<n2)
+# print(n1==n2)
+
+#----logical operator
+'''
+     c1   c2    f
+AND  T    T     T
+     T    F     F
+     F    T     F
+     F    F     F
+
+     c1   c2    f
+OR   T    T     T
+     T    F     T
+     F    T     T
+     F    F     F     
+
+     
+NOT    
+'''
+
+# marks=eval(input('enter your marks:'))
+# attendance=eval(input('enter attandence here:'))
+
+# # print(marks>80)
+# # print(attendance>75)
+# print(marks>80 and attendance>75 ) 
+# print(marks>80 or attendance>75 )
+
+#----membership operator
+#1.IN operator
+
+# number=[10,20,30,40,50,60]
+# print(30 in number)
+# print(35 in number)
+# name='ravi pawar'
+# print('r' in name)
+# print('s' in name)
+
+#2.NOT IN operator
+
+# number=[10,20,30,40,50,60]
+# print(30 not in number)
+# print(35 not in number)
+# name='ravi pawar'
+# print('r' not in name)
+# print('s' not in name)
+
+#---identity operater
+# 1.is operator
+
+# a=100
+# b=100
+# print(a==b) #it will only check or compare the value
+# print(a is b) #it will check the memory of both object if same they will return true otherwise false
+
+# l1=[10,20,30]
+# l2=[10,20,30]
+# print(l1==l2)
+# print(l1 is l2)
+
+
+#2.is not operator
+
+# a=100
+# b=100
+# print(a==b)
+# print(a is not b)
+
+# l1=[10,20,30]
+# l2=[10,20,30]
+# print(l1==l2)
+# print(l1 is not l2)
+
+
+
+# l1=[10,200,930,40]
+# l1.sort()
+# l1.sorted(l1)
+# print(l1)
+
+
+#---Assisgnment operater
+
+
+
+
